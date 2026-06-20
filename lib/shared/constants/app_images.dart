68836b0c711fa58c96b98ee5c3f8fe65d5f44226
duynho11-cmd <nhoduy11@gmail.com>
2026-06-21 02:21:03 +0700

    update

--- lib/shared/constants/app_images.dart
+++
@@ -1,7 +0,0 @@
-class AppImages {
-  static const logo = "assets/images/LogoKing.png";
-
-  static const onboarding1 = "assets/images/ob1.png";
-  static const onboarding2 = "assets/images/ob2.png";
-  static const onboarding3 = "assets/images/ob3.png";
-}
